529116f609ecdb7e900f994b366c3a5377e50d4d
terrancesnyder <terrance.a.snyder@gmail.com>
2023-11-14 11:00:49 +0900

    z

--- src/routes/consent.ts
+++ src/routes/consent.ts
@@ -137,7 +137,9 @@ async function createOAuth2ConsentRequestSession(
       let m = (identity?.metadata_public as any);
       // access_token.locale = id_token.locale = identity?.metadata_public?.locale;
       // access_token.preferred_username = id_token.preferred_username = identity.metadata_public?['preferred_username'] : '';
-      access_token.picture = id_token.picture = m.picture!;
+      if (m.picture != null) {
+        access_token.picture = id_token.picture = m.picture;
+      }
       // access_token.gender = id_token.gender = identity.metadata_public?['gender'] : '';
     }
     if (grantScopes.indexOf("email") > -1) {
